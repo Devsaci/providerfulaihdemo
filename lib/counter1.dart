46e77fca905632cd0ab32bcc505d8114d36570a2
saci zakaria <sacideveloppeur@gmail.com>
2021-11-29 11:37:46 +0100

    Initial commit

---
+++ lib/counter1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Counter1 extends StatelessWidget {
+  const Counter1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      color: Colors.deepOrangeAccent,
+      elevation: 6,
+      child: SizedBox(
+        width: 150,
+        height: 150,
+        child: Center(
+          child: Text("0", style: TextStyle(fontSize: 30)),
+        ),
+      ),
+    );
+  }
+}
