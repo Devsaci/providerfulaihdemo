fb43ad80842794231a431d7dbe90df3fa24f30d6
saci zakaria <sacideveloppeur@gmail.com>
2021-11-30 19:26:07 +0100

    MyProvider

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChangeNotifierProvider(
+      home: ChangeNotifierProvider<MyProvider>(
         create: (_) => MyProvider(),
         child: CounterPro(),
       ),
